1f69b495af28cf609aa57d7d001e2d9e2d75f7d8
Eric Harrison <eric@getdocket.co>
2019-12-29 20:42:33 -0600

    Adds a meg!

--- bot.js
+++ bot.js
@@ -1,3 +1,72 @@
 
 const options = {
+    options: {
+        debug: true
+    },
+    connection: {
+        reconnect: true
+    },
+    identity: {
+        username: process.env.USERNAME,
+        password: process.env.OAUTH
+    },
+    channels: ['MegMegalodon']
 };
+
+const prefix = '!';
+
+const tmi = require('tmi.js');
+
+const client = new tmi.client(options);
+client.connect();
+
+let activeMeg = false;
+let megHealth = 1000;
+
+function random(low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
+
+client.on('chat', (channel, userstate, message, self) => {
+
+    if ( ! message.startsWith(prefix) || self ) {
+        return;
+    }
+
+    const args = message.slice(prefix.length).split(/ +/);
+    const cmd  = args.shift().toLowerCase();
+
+    switch (cmd) {
+
+        case 'test': 
+            client.say('channel', `@${userstate.username} - Sherman is sailing...`);
+        break;
+
+        case 'fire':
+
+            if ( activeMeg ) {
+                let damage = random(0,200);
+                megHealth = megHealth - damage;
+
+                client.say('channel', `@${userstate.username} shot at the Meg and did ${damage} damage.`);
+
+                if ( megHealth < 0 ) {
+                    client.say('The Meg has been slain! Collect your rewards!');
+                    megHealth = 1000;
+                    activeMeg = false;
+                }
+            }
+
+        break;
+
+        case 'meg':
+
+            if ( userstate.username == 'tehblister' ) {
+                client.say('channel', 'A wild Meg appears! Kill it with cannons! [!fire]');
+            }
+
+        break;
+
+    }
+
+});
