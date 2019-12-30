45f26b122f848c1320e7ae0ae08446153494f3e1
Eric Harrison <eric@getdocket.co>
2019-12-29 21:11:23 -0600

    Switch back to chat bot

--- bot.js
+++ bot.js
@@ -15,10 +15,18 @@ const options = {
 
 const prefix = '!';
 
-const tmi = require('tmi.js');
+//const tmi = require('tmi.js');
 
-const client = new tmi.client(options);
-client.connect();
+//const client = new tmi.client(options);
+//client.connect();
+
+const TwitchBot = require('twitch-bot');
+
+const bot = new TwitchBot({
+    username: process.env.USERNAME,
+    oauth: process.env.OAUTH,
+    channels: ['MegMegalodon']
+});
 
 let activeMeg = false;
 let megHealth = 1000;
@@ -27,19 +35,21 @@ function random(low, high) {
     return Math.floor(Math.random() * (high - low) + low);
 }
 
-client.on('chat', (channel, userstate, message, self) => {
+bot.on('message', chatter => {
 
-    if ( ! message.startsWith(prefix) || self ) {
+    console.log(`#${chatter.channel} ${chatter.username}: ${chatter.message}`);
+
+    if ( ! chatter.message.startsWith(prefix) || chatter.username == 'shermanthebot' ) {
         return;
     }
 
-    const args = message.slice(prefix.length).split(/ +/);
+    const args = chatter.message.slice(prefix.length).split(/ +/);
     const cmd  = args.shift().toLowerCase();
 
     switch (cmd) {
 
         case 'test': 
-            client.say('MegMegalodon', `@${userstate.username} - Sherman is sailing...`);
+            Bot.say(`@${chatter.username} - Sherman is sailing...`);
         break;
 
         case 'fire':
@@ -48,10 +58,10 @@ client.on('chat', (channel, userstate, message, self) => {
                 let damage = random(0,200);
                 megHealth = megHealth - damage;
 
-                client.say('MegMegalodon', `@${userstate.username} shot at the Meg and did ${damage} damage.`);
+                Bot.say(`@${chatter.username} shot at the Meg and did ${damage} damage.`);
 
                 if ( megHealth < 0 ) {
-                    client.say('MegMegalodon', 'The Meg has been slain! Collect your rewards!');
+                    Bot.say('The Meg has been slain! Collect your rewards!');
                     megHealth = 1000;
                     activeMeg = false;
                 }
@@ -61,8 +71,8 @@ client.on('chat', (channel, userstate, message, self) => {
 
         case 'meg':
 
-            if ( userstate.username == 'tehblister' ) {
-                client.say('MegMegalodon', 'A wild Meg appears! Kill it with cannons! [!fire]');
+            if ( chatter.username == 'tehblister' ) {
+                Bot.say('A wild Meg appears! Kill it with cannons! [!fire]');
             }
 
         break;
