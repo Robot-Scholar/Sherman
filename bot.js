475df822ad1b91b38cf452c464e8575215930e6a
Eric Harrison <eric@getdocket.co>
2019-12-29 23:13:25 -0600

    Adds krakens and treasure

--- bot.js
+++ bot.js
@@ -31,7 +31,9 @@ const Bot = new TwitchBot({
 });
 
 let activeMeg = false;
+let activeKraken = false;
 let megHealth = 1000;
+let krakenHealth = 5000;
 
 function random(low, high) {
 	return Math.floor(Math.random() * (high - low) + low);
@@ -40,12 +42,27 @@ function random(low, high) {
 let shipHoles = 0;
 let shipWater = 0;
 
+let Players = {};
+
 Bot.on('message', chatter => {
 
 	console.log(`${chatter.channel} ${chatter.username}: ${chatter.message}`);
 
 	if ( ! chatter.message.startsWith(prefix) || chatter.username == 'shermanthebot' ) {
 		return;
+	}
+
+	if ( chatter.username in Players ) {
+		Players[ chatter.username ]['doubloons']++;
+	} else {
+		Players[ chatter.username ] = {
+			doubloons: 1,
+			megs: 0,
+			kraken: 0,
+			kills: 0,
+			repairs: 0,
+			bails: 0
+		};
 	}
 
 	const args = chatter.message.slice(prefix.length).split(/ +/);
@@ -63,15 +80,52 @@ Bot.on('message', chatter => {
 				let damage = random(0,400);
 				megHealth = megHealth - damage;
 
+				Players[ chatter.username ]['doubloons'] += damage;
 				Bot.say(`@${chatter.username} shot at the Meg and did ${damage} damage.`);
 
 				if ( megHealth < 0 ) {
 					Bot.say('The Meg has been slain! Collect your rewards!');
 					megHealth = 1000;
 					activeMeg = false;
+
+					Players[ chatter.username ]['doubloons'] += 500;
+					Players[ chatter.username ]['kills'] += 1;
+					Players[ chatter.username ]['megs'] += 1;
+				}
+			} else if ( activeKraken ) {
+				let damage = random(0,400);
+				krakenHealth = krakenHealth - damage;
+
+				Players[ chatter.username ]['doubloons'] += damage;
+				Bot.say(`@${chatter.username} shot at a tentacle and did ${damage} damage.`);
+
+				if ( krakenHealth < 0 ) {
+					Bot.say('The Kraken has been slain and sinks beneath the waves! Collect your rewards!');
+					krakenHealth = 1000;
+					krakenMeg = false;
+
+					Players[ chatter.username ]['doubloons'] += 5000;
+					Players[ chatter.username ]['kills'] += 1;
+					Players[ chatter.username ]['krakens'] += 1;
 				}
 			}
 
+		break;
+
+		case 'megs':
+			if ( Players[ chatter.username ]['megs'] == 0 ) {
+				Bot.say(`You haven't killed any Megalodons, @${chatter.username}`);
+			} else {
+				Bot.say(`You've murdered ${Players[ chatter.username ]['megs']} Megalodons', @${chatter.username}`);
+			}
+		break;
+
+		case 'krakens':
+			if ( Players[ chatter.username ]['krakens'] == 0 ) {
+				Bot.say(`You haven't killed any krakens, @${chatter.username}`);
+			} else {
+				Bot.say(`You've slain ${Players[ chatter.username ]['krakens']} krakii (is this a word?)', @${chatter.username}`);
+			}
 		break;
 
 		case 'bail':
@@ -80,8 +134,12 @@ Bot.on('message', chatter => {
 				return;
 			}
 			if ( shipWater > 0 ) {
+				Players[ chatter.username ]['doubloons'] += 25;
 				shipWater = shipWater - 1;
 				Bot.say(`${chatter.username} vigorously bails water...`);
+
+				Players[ chatter.username ]['bails'] += 1;
+
 				return;
 			}
 		break;
@@ -91,11 +149,18 @@ Bot.on('message', chatter => {
 				Bot.say(`${chatter.username} should not be allowed to play with wood...`)
 			}
 			if ( shipHoles > 0 ) {
+				Players[ chatter.username ]['repairs'] += 1;
+				Players[ chatter.username ]['doubloons'] += 50;
 				shipHoles = shipHoles - 1;
 				Bot.say('The sound of hammering below decks is reassuring.');
 				return;
 			}
 		
+		break;
+
+		case 'treasure':
+			Bot.say(`${chatter.username}, you have earned ${Players[chatter.username]['doubloons']} doubloons! Make sure the ship makes it to an outpost!`);
+			return;
 		break;
 
 		case 'milk':
@@ -107,7 +172,7 @@ Bot.on('message', chatter => {
 			if ( chatter.username == 'tehblister' ) {
 				activeMeg = true;
 				megHealth = random(500, 2500);
-				Bot.say('A wild Meg appears! Kill it with cannons! [!fire]');
+				Bot.say('A wild Meg appears! Kill it with cannons! !fire away');
 
 				setTimeout(function() { 
 
@@ -123,6 +188,18 @@ Bot.on('message', chatter => {
 
 		break;
 
+		case 'kraken':
+
+			if ( chatter.username == 'tehblister' ) {
+				activeKraken = true;
+				krakenHealth = random(1500, 2500);
+				Bot.say('A kraken rises from the depths... open !fire');
+
+				setTimeout(crushKraken, 1000 * random(10, 70));
+			}
+
+		break;
+
 	}
 
 });
@@ -132,9 +209,21 @@ function chargeMeg() {
 		Bot.say('The Meg is charging!');
 		shipHoles = shipHoles + 1;
 	
-		console.log('Damage Check... ' + shipHoles + ':' + shipWater);
+		console.log('Damage Check... ' + shipHoles + ':' + shipWater + ' - [/Meg:' + megHealth + ']');
 	
 		setTimeout(chargeMeg, 1000 * random(10, 40));
+	}
+}
+
+
+function crushKraken() {
+	if ( activeKraken ) {
+		Bot.say('The kraken is crushing your ship!');
+		shipHoles = shipHoles + 3;
+	
+		console.log('Damage Check... ' + shipHoles + ':' + shipWater + ' - [kraken:' + krakenHealth + ']');
+	
+		setTimeout(crushKraken, 1000 * random(10, 70));
 	}
 }
 
@@ -151,13 +240,15 @@ function checkDamage() {
 		Bot.say('Your ship sank! Everyone loses all their treasure. megmeg2Rip');
 		shipWater = 0;
 		shipHoles = 0;
+
+		for ( let player in Players ) {
+			Players[ player ]['doubloons'] = 0;
+		}
 	}
 	
-	console.log('Damage Check... ' + shipHoles + ':' + shipWater);
+	console.log('Damage Check... ' + shipHoles + ':' + shipWater + ' - [kraken:' + krakenHealth + '/Meg:' + megHealth + ']');
 
-	if ( activeMeg ) {
-		setTimeout(checkDamage, 10000);
-	}
+	setTimeout(checkDamage, 10000);
 	
 }
 
